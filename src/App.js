b224b12f7b95ddf302a3203387fe1f9039201222
Numan AHmad <na56439@gmail.com>
2022-05-02 12:03:29 +0500

    create new navbar and add some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div className="default">NavBar</div>;
+  return (
+    <>
+      <div className="nav-fixed">
+        <div className="nav-container">
+          <div className="nav-heading">
+            <h3>MNA'S</h3>
+          </div>
+          <div className="search-bar">
+            <input type="search" />
+          </div>
+          <div className="menu-container">
+            <div className="menu">About Us</div>
+            <div className="menu">Contact Us</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
